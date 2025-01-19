a195f26419a078717d89345a5bfcfbeaca8b180a
kipland-m <kiplandmelton@gmail.com>
2025-01-19 16:48:36 -0600

    working writing to a file 'test.txt'

--- src/main.c
+++ src/main.c
@@ -1,11 +1,37 @@
 #include <stdio.h>
-char buffer[1024] = {0};
+#include <string.h>
+
+void edit_line(char* buffer, int current_line){
+  int i;
+  for (i = 0; i < current_line; i++){
+    buffer = strchr(buffer, '\n') + 1;
+  }
+
+  char* line_end = strchr(buffer, '\n');
+  char saved[1024] = {0};
+
+  strcpy(saved, line_end);
+  scanf("%s", buffer);
+  strcpy(buffer + strlen(buffer), saved);
+}
 
 int main(int argc, char** argv){
+  char buffer[1024] = {0};
   FILE* f = fopen(argv[1], "r");
 
   fread(buffer, 1024, 1, f);
   fclose(f);
 
+  printf("Contents:\n%s\n", buffer);
+
+  int current_line = 0;
+  scanf("%d", &current_line);
+  edit_line(buffer, current_line);
+
+  f = fopen(argv[1], "w");
+  fwrite(buffer, strlen(buffer), 1, f);
+  fclose(f);
+
   return 0;
 }
+
